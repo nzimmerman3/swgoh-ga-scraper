5d4cae2c7d5704a8831a25acc604133c1cbfba0f
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2020-09-29 13:01:50 -0700

    Added a second layer to crawler that follows links

--- swgoh/swgoh/spiders/ga_spider.py
+++ swgoh/swgoh/spiders/ga_spider.py
@@ -8,30 +8,43 @@ def printAll(ls):
 
 class GaSpider(scrapy.Spider):
 
-    defenses = {
-        1: 9,
-        2: 9,
-        3: 7,
-        4: 7,
-        5: 6,
-        6: 6,
-        7: 5,
-        8: 4,
-        9: 4,
-        10: 4,
-        11: 4
+    max_banners = {
+        1: 8 * 64 + 72,
+        2: 8 * 64 + 72,
+        3: 6 * 64 + 72,
+        4: 6 * 64 + 72,
+        5: 5 * 64 + 72,
+        6: 5 * 64 + 72,
+        7: 4 * 64 + 72,
+        8: 3 * 64 + 72,
+        9: 3 * 64 + 72,
+        10: 3 * 64 + 72,
+        11: 3 * 64 + 72
     }
 
     name = "ga"
 
+    # allycode => (avg banners, win percentage, cleared percentage)
+    results = {}
+
     def start_requests(self):
         urls = [
-            'https://swgoh.gg/p/146523987/gac-history/?gac=34&r=1',
-            'https://swgoh.gg/p/146523987/gac-history/?gac=33',
-            'https://swgoh.gg/p/146523987/gac-history/?gac=34&r=3&a=d'
+            'https://swgoh.gg/g/68989/the-senate-ksdagobahks/'
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.init)
+
+    def init(self, response):
+        # todo need to parse request and find urls
+        print("Results:")
+        urls = ["https://swgoh.gg/" + x +
+                "gac-history/?gac=34&r=1" for x in response.css('a::attr(href)').re(r'/p/\d{9}/')]
+
+        for url in urls:
+            # yield scrapy.Request(url=url, callback=self.parse)
+            print(url)
+
+        yield scrapy.Request(url=urls[0], callback=self.parse)
 
     def parse(self, response):
         print("Start")
@@ -40,8 +53,13 @@ class GaSpider(scrapy.Spider):
             '.btn.btn-default.dropdown-toggle::text').re(r'.+')
         banners = response.css('.gac-summary--win::text').re(r'Banners: (\d+)')
         battle_results = response.css('.gac-summary__status::text').re(r'\w+')
+        banners = response.css('.m-a-0::text').re(r'(\d+) - \d+')
+        print(banners)
 
+        print(response.css('.m-a-0::text').get())
         printAll(ga_info)
+        ally = response.request.url.split('/')[-3]
+        print(ally)
 
         ga_battles = []
         i = 0
@@ -53,8 +71,10 @@ class GaSpider(scrapy.Spider):
 
         avg_banners = sum(ga_battles) / len(ga_battles)
         win_percentage = battle_results.count("WIN") / len(battle_results)
+        # banner_percentage =
 
-        print("Avg. Banners: %d" % avg_banners)
-        print("")
+        print("Average Banners: %d" % avg_banners)
+        print("Win Percentage: %d" % win_percentage)
+        # prints("Banner Pecentage %d%" % banner_percentage)
 
         print("End")
